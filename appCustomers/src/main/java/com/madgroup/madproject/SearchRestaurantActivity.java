5661e87235ddd24ac2e4f12819efe3c39b449f0c
Ennio <ennio.riccobene@gmail.com>
2019-04-12 20:58:40 +0200

    Customers Restaurant layout and basic activity created

---
+++ appCustomers/src/main/java/com/madgroup/madproject/SearchRestaurantActivity.java
@@ -0,0 +1,16 @@
+package com.madgroup.madproject;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchrestaurant);
+
+    }
+}
