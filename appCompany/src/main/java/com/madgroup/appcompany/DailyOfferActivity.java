36e759e5f2d23f47084c2f6c165c835ae00363e7
raffaelemartone <raffaele.martone22@icloud.com>
2019-04-12 17:46:16 +0200

    Integrato camera/galleria/permessi nella Dialog

--- appCompany/src/main/java/com/madgroup/appcompany/DailyOfferActivity.java
+++ appCompany/src/main/java/com/madgroup/appcompany/DailyOfferActivity.java
@@ -1,23 +1,30 @@
 package com.madgroup.appcompany;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Base64;
@@ -26,17 +33,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+import com.madgroup.sdk.MyImageHandler;
+import com.madgroup.sdk.SmartLogger;
+import com.yalantis.ucrop.UCrop;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class DailyOfferActivity extends AppCompatActivity implements
-        NavigationView.OnNavigationItemSelectedListener {
+        NavigationView.OnNavigationItemSelectedListener, PopupMenu.OnMenuItemClickListener {
 
     public static final int THUMBSIZE = 50;
     private NavigationView navigationView;
@@ -46,6 +59,12 @@ public class DailyOfferActivity extends AppCompatActivity implements
     private RecyclerView recyclerView;
     private DailyOfferRecyclerViewAdapter adapter;
     private Dish currentDish;
+    private static String POPUP_CONSTANT = "mPopup";
+    private static String POPUP_FORCE_SHOW_ICON = "setForceShowIcon";
+    private static final int CAMERA_PERMISSIONS_CODE = 1;
+    private static final int GALLERY_PERMISSIONS_CODE = 2;
+    public int iteration = 0;
+    private Dialog currentDialog;
 
     ArrayList<Dish> myList = new ArrayList<>();
 
@@ -81,9 +100,17 @@ public class DailyOfferActivity extends AppCompatActivity implements
 
     private void initDailyOfferRecyclerView() {
         recyclerView = this.findViewById(R.id.my_recycler_view);
-        adapter = new DailyOfferRecyclerViewAdapter(this,  myList);
+        adapter = new DailyOfferRecyclerViewAdapter(this, myList);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Serve per modificare il campo currentDialog dall'adapter
+        adapter.adapterhandler = new AdapterHandler() {
+            @Override
+            public void setCurrentDialog(Dialog dialog) {
+                DailyOfferActivity.this.currentDialog = dialog;
+            }
+        };
 
     }
 
@@ -188,13 +215,13 @@ public class DailyOfferActivity extends AppCompatActivity implements
         return super.onOptionsItemSelected(item);
     }
 
-
     private void showDialog() {
         // custom dialog
         final Dialog dialog = new Dialog(this);
         dialog.setTitle(R.string.edit_dish);
         dialog.setCancelable(true);
         dialog.setContentView(R.layout.custom_dish_dialog);
+        currentDialog = dialog;
 
         ImageButton dialogDismiss = (ImageButton) dialog.findViewById(R.id.dialogDismiss);
         ImageButton dialogConfirm = (ImageButton) dialog.findViewById(R.id.dialogConfirm);
@@ -234,7 +261,6 @@ public class DailyOfferActivity extends AppCompatActivity implements
                     floatPrice = Float.parseFloat(floatStringVal);
                 }
 
-
                 if (editDishName.getText().toString().isEmpty() || editDishQuantity.getText().toString().isEmpty()) {
                     Toast.makeText(getApplicationContext(), getString(R.string.requiredString), Toast.LENGTH_SHORT).show();
                 } else if (floatPrice==0) {
@@ -252,11 +278,166 @@ public class DailyOfferActivity extends AppCompatActivity implements
                     adapter.notifyItemRangeChanged(myList.size() - 1, myList.size());
                     dialog.dismiss();
                 }
-
             }
         });
-
         dialog.show();
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        try {
+            // Reflection apis to enforce show icon
+            Field[] fields = popup.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                if (field.getName().equals(POPUP_CONSTANT)) {
+                    field.setAccessible(true);
+                    Object menuPopupHelper = field.get(popup);
+                    Class<?> classPopupHelper = Class.forName(menuPopupHelper.getClass().getName());
+                    Method setForceIcons = classPopupHelper.getMethod(POPUP_FORCE_SHOW_ICON, boolean.class);
+                    setForceIcons.invoke(menuPopupHelper, true);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // This activity implements OnMenuItemClickListener
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.profile_camera_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemCamera:
+                int cameraPermission = ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.CAMERA);
+                boolean userPreviousDeniedRequest = ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.CAMERA);
+
+                if (cameraPermission== PackageManager.PERMISSION_GRANTED) {
+                    MyImageHandler.getInstance().startCamera(this);
+                } else {
+                    if (userPreviousDeniedRequest) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.camerapermission), Toast.LENGTH_SHORT).show();
+                    } else {
+                        ActivityCompat.requestPermissions(this,
+                                new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSIONS_CODE);
+                    }
+                }
+                return true;
+
+            case R.id.itemGallery:
+
+                int readStoragePermission = ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE);
+                boolean userPreviousDeniedGalleryRequest = ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                if (readStoragePermission==PackageManager.PERMISSION_GRANTED) {
+                    MyImageHandler.getInstance().startGallery(this);
+                } else {
+                    if (userPreviousDeniedGalleryRequest) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.gallerypermission), Toast.LENGTH_SHORT).show();
+                    } else {
+                        ActivityCompat.requestPermissions(this,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, GALLERY_PERMISSIONS_CODE);
+                    }
+                }
+                return true;
+
+            case R.id.itemDelete:
+                // Set the default image
+                Drawable defaultImg = getResources().getDrawable(R.drawable.ic_dish);
+                CircleImageView dishImage = (CircleImageView) currentDialog.findViewById(R.id.dishImage);
+                dishImage.setImageDrawable(defaultImg);
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        /* Retreiving the tumbnail: the Android Camera application encodes the photo in the return Intent
+        delivered to onActivityResult() as a small Bitmap in the extras, under the key "data" */
+        if (requestCode == MyImageHandler.Camera_Pick_Code && resultCode == RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            CircleImageView dishImage = (CircleImageView) currentDialog.findViewById(R.id.dishImage);
+            dishImage.setImageBitmap(bitmap);
+        }
+
+        if (requestCode == MyImageHandler.Gallery_Pick_Code && resultCode == RESULT_OK && data != null) {
+            try {
+                Uri selectedImageUri = data.getData();
+                /*
+                    Getting the result and show it in the ImageButton
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
+                    personalImage.setImageBitmap(bitmap);
+                */
+                if (selectedImageUri != null) {
+                    MyImageHandler.getInstance().startCrop(selectedImageUri, iteration, this, this);
+                    iteration++;
+                }
+            } catch (Exception e) {
+                SmartLogger.e("Error in selectedImageUri: " + e.getMessage());
+            }
+        }
+        if (requestCode == UCrop.REQUEST_CROP) {
+            if (data!=null)
+                handleCropResult(data);
+        }
+    }
+
+    //  Richiamata dopo il crop
+    private void handleCropResult(@NonNull Intent result) {
+        final Uri uri = UCrop.getOutput(result);
+        if (uri != null) {
+            try {
+                CircleImageView dishImage = (CircleImageView) currentDialog.findViewById(R.id.dishImage);
+                dishImage.setImageURI(uri);
+            } catch (Exception e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_PERMISSIONS_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+                    MyImageHandler.getInstance().startCamera(this);
+                } else {
+                    // permission denied!
+                    Toast.makeText(getApplicationContext(), getString(R.string.camerapermission), Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            case GALLERY_PERMISSIONS_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    MyImageHandler.getInstance().startGallery(this);
+                } else {
+                    // permission denied!
+                    Toast.makeText(getApplicationContext(), getString(R.string.gallerypermission), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    // Serve per modificare il campo currentDialog dall'adapter
+    public abstract class AdapterHandler
+    {
+        public void setCurrentDialog(Dialog dialog) {}
     }
 
 }
