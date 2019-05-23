aa51ddac1dcb13761a3284e199096890086dc7e3
Ennio <ennio.riccobene@gmail.com>
2019-05-23 13:03:03 +0200

    Fatto il layout di Evaluation Activity

--- appCustomers/src/main/java/com/madgroup/madproject/EvaluationActivity.java
+++ appCustomers/src/main/java/com/madgroup/madproject/EvaluationActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RatingBar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.madgroup.sdk.Reservation;
 
 public class EvaluationActivity extends AppCompatActivity {
+
+    RatingBar restaurantBar;
+    RatingBar foodBar;
+    RatingBar serviceBar;
+    FloatingActionButton sendBtn;
 
     public static void start(Context context, String orderId, String restaurantId, String customerId, String bikerId) {
         Intent starter = new Intent(context, EvaluationActivity.class);
@@ -23,5 +30,12 @@ public class EvaluationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluation);
+
+        setTitle(R.string.RatingTitle);
+        restaurantBar = findViewById(R.id.ratingRestaurant);
+        foodBar = findViewById(R.id.ratingFood);
+        serviceBar = findViewById(R.id.ratingService);
+        sendBtn = findViewById(R.id.sendReview);
+
     }
 }
