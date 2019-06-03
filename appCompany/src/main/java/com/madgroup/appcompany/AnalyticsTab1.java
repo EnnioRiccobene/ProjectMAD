a1b19514be144f0cdaa859088344a677d44e66f1
raffaelemartone <raffaele.martone22@icloud.com>
2019-06-03 23:40:30 +0200

    selezione daily

--- appCompany/src/main/java/com/madgroup/appcompany/AnalyticsTab1.java
+++ appCompany/src/main/java/com/madgroup/appcompany/AnalyticsTab1.java
@@ -3,23 +3,35 @@ package com.madgroup.appcompany;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.RectF;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.IMarker;
+import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.MPPointF;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -42,7 +54,7 @@ import java.util.TreeMap;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
-public class AnalyticsTab1 extends Fragment {
+public class AnalyticsTab1 extends Fragment implements OnChartValueSelectedListener  {
 
     private OnFragmentInteractionListener mListener;
     private SharedPreferences.Editor editor;
@@ -54,7 +66,8 @@ public class AnalyticsTab1 extends Fragment {
     private TextView currentFilter;
     private TextView salesTextView;
     private TextView topDishName;
-
+    private BarChart chart = null;
+    private TextView descriptionChart;
 
     public AnalyticsTab1() {
         // Required empty public constructor
@@ -67,6 +80,36 @@ public class AnalyticsTab1 extends Fragment {
         prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
         editor = prefs.edit();
         initMapMonths();
+    }
+
+
+    /* Metodi per l'interazione con il BarChart */
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        if (e == null)
+            return;
+
+        String slotHour = Integer.toString(Math.round(h.getX()));
+        String startingHour = Integer.toString(Math.round(h.getX())) + ":00";
+        String endingHour = "";
+        if (!startingHour.equals("23:00"))
+            endingHour = endingHour + Integer.toString(Math.round(h.getX() + 1)) + ":00.";
+        else
+            endingHour = endingHour + "00:00.";
+
+        Integer totalSales = Math.round(h.getY());
+        if (totalSales==0) {
+            descriptionChart.setText("No sales detected from " + startingHour + " to " + endingHour);
+        } else {
+            String startingString = totalSales + " sales detected from " + startingHour + " to " + endingHour;
+            setDescriptionChart(startingString, slotHour, selectedDay, selectedMonth, selectedYear);
+        }
+
+
+    }
+    @Override
+    public void onNothingSelected() {
+
     }
 
     private void initMapMonths() {
@@ -130,6 +173,7 @@ public class AnalyticsTab1 extends Fragment {
         mListener = null;
     }
 
+
     public interface OnFragmentInteractionListener {
 
     }
@@ -137,7 +181,8 @@ public class AnalyticsTab1 extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        final BarChart chart = view.findViewById(R.id.bar_chart);
+        chart = view.findViewById(R.id.bar_chart);
+        descriptionChart = view.findViewById(R.id.description_chart);
         ImageView previousButton = view.findViewById(R.id.previous_button);
         ImageView nextButton = view.findViewById(R.id.next_button);
         currentFilter = view.findViewById(R.id.current_filter);
@@ -265,14 +310,80 @@ public class AnalyticsTab1 extends Fragment {
                 chart.setPinchZoom(false);
                 chart.setDoubleTapToZoomEnabled(false);
 
+                chart.setOnChartValueSelectedListener(AnalyticsTab1.this);
+//                MyMarkerView marker = new MyMarkerView(getContext(), R.layout.marker_layout);
+//                marker.setChartView(chart);
+//                chart.setMarker(marker);
+
                 chart.invalidate(); // refresh
                 chart.animateY(1000);
+
 
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
+    }
+
+    private void setDescriptionChart(final String startingString, final String slotHour, final String dayOfMonth, final String month, final String year) {
+
+        // Database references
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final String restaurantID = prefs.getString("currentUser", "");
+
+        String weekOfMonth = getWeekOfMonth(dayOfMonth, month, year);
+
+        String node = year+"_"+month+"_"+weekOfMonth;
+        DatabaseReference topMealRef = database.getReference().child("Analytics").child("TopMeals")
+                .child(restaurantID).child(node);
+
+
+        String hourSlotString = dayOfMonth + "_" + getDayOfWeek(dayOfMonth, month, year) + "_" + slotHour;
+        DatabaseReference selectedHourSlot = topMealRef.child(hourSlotString);
+
+        final String[] topMealOfHourSlot = {""};
+        selectedHourSlot.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Integer maxSales = 0;
+                // Per ogni piatto relativa alla fascia oraria
+                for (DataSnapshot dishIDsales : dataSnapshot.getChildren()) {
+                    Integer sale = dishIDsales.getValue(Integer.class);
+                    if (sale > maxSales) {
+                        maxSales = sale;
+                        topMealOfHourSlot[0] = dishIDsales.getKey();
+                    }
+                }
+                DatabaseReference ref = database.getReference().child("Company").child("Menu")
+                        .child(restaurantID).child(topMealOfHourSlot[0]);
+                final Integer finalMaxSales = maxSales;
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Dish dish = dataSnapshot.getValue(Dish.class);
+                        String dishName = dish.getName();
+                        String bestMealString = "\nTop meal: " + dishName + "("+dish.getPrice()+"), "+ Integer.toString(finalMaxSales) + " sales.";
+                        descriptionChart.setText(startingString + bestMealString);
+
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
     }
 
 
@@ -356,6 +467,22 @@ public class AnalyticsTab1 extends Fragment {
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
+    }
+
+    private String getDayOfWeek(String dayOfMonth, String month, String year) {
+        String input = year+"/"+month+"/"+dayOfMonth;
+        String format = "yyyy/MM/dd";
+        SimpleDateFormat df = new SimpleDateFormat(format);
+        Date date = null;
+        try {
+            date = df.parse(input);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        Integer weekOfMonth = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        return Integer.toString(weekOfMonth);
     }
 
     @NotNull
